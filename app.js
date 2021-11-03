a42cd8bf6d56f4cc0ffca298912ef6c8f48b225b
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2021-11-03 16:46:57 +0330

    changed the folders structure

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // Add the public folder as static for serving files
 app.use(express.static(path.join(__dirname, "public")));
-// Add the todo folder as static for serving todo files in the /todo url
-app.use("/todo", express.static(path.join(__dirname, "todo")));
-// Add the calculator folder as static for serving calculator files in the /calculator url
-app.use("/calculator", express.static(path.join(__dirname, "calculator")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
